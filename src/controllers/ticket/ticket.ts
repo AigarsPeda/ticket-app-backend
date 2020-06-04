1c8fa8975769bcf022d1ac3ee9d1769202e5ca6b
Aigars Peda <aigarspeda@gmail.com>
2020-06-04 11:38:03 +0300

    class Ticket

---
+++ src/controllers/ticket/ticket.ts
@@ -0,0 +1,31 @@
+import { ITicket } from "./../../interface/ticket.interface";
+import { Context } from "koa";
+import Joi from "@hapi/joi";
+import { TicketModel } from "../../models/ticket/Ticket.model";
+import { UserModel } from "../../models/user/User.model";
+
+const RANDOM_VALUE_MULTIPLIER = 10001;
+
+export class Ticket {
+  public async addTicket(ctx: Context): Promise<void> {
+    try {
+      const body: ITicket = ctx.request.body;
+      const schema = Joi.object().keys({
+        fullName: Joi.string().required(),
+        email: Joi.string().required(),
+        subject: Joi.string().required(),
+        description: Joi.string().required(),
+        department: Joi.string().required(),
+        priority: Joi.string().required(),
+      });
+      const value: ITicket = await schema.validateAsync(body);
+      const { id } = ctx.state.user;
+      value.user = id;
+      value.tickedId = `${Math.floor(Math.random() * RANDOM_VALUE_MULTIPLIER)}`;
+      const ticket = await TicketModel.create(value);
+    } catch (error) {
+      ctx.body = error;
+      console.log("Error addTicket: ", error);
+    }
+  }
+}
