28d188374651c85b7419dec5091cb469ef6e288b
Aigars Peda <aigarspeda@gmail.com>
2020-06-06 19:13:35 +0300

    ticket routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,7 @@
 import Router from "koa-router";
 import { Auth } from "./controllers/auth/auth";
+import { Ticket } from "./controllers/ticket/ticket";
+import { verifyToken } from "./helpers/auth";
 
 export function registerRoutes() {
   const router = new Router();
@@ -9,6 +11,10 @@ export function registerRoutes() {
   // AUTH ROUTE
   router.post("/register", Auth.prototype.create);
   router.post("/login", Auth.prototype.login);
+
+  // TICKET ROUTES
+  router.get("/tickets", verifyToken, Ticket.prototype.getAllTickets);
+  router.post("/tickets", verifyToken, Ticket.prototype.addTicket);
 
   return router;
 }
