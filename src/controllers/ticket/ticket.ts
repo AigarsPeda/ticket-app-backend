6753009f57669edb81088468c14fa4dc6e3067b6
Aigars Peda <aigarspeda@gmail.com>
2020-06-07 07:46:57 +0300

    deleteTicket

--- src/controllers/ticket/ticket.ts
+++ src/controllers/ticket/ticket.ts
@@ -86,4 +86,28 @@ export class Ticket {
       ctx.body = error;
     }
   }
+
+  public async deleteTicket(ctx: Context): Promise<void> {
+    try {
+      const { _id } = ctx.params;
+      const { id } = ctx.state.user;
+      await TicketModel.deleteOne({ _id });
+      await UserModel.updateOne(
+        {
+          _id: id,
+        },
+        {
+          $pull: {
+            tickets: {
+              ticket: _id,
+            },
+          },
+        }
+      );
+      ctx.body = { message: "Ticket deleted successfully" };
+    } catch (error) {
+      console.log("ERROR deleteTicket: ", error);
+      ctx.body = error;
+    }
+  }
 }
